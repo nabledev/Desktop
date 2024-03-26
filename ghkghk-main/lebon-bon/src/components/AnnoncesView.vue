d055981c5325e627851b57156bcdcc74b6d7a17d
rusafae <rusafae@D350-S15>
2024-03-26 11:16:39 +0100

    eyrey

--- ghkghk-main/lebon-bon/src/components/AnnoncesView.vue
+++ ghkghk-main/lebon-bon/src/components/AnnoncesView.vue
@@ -1,13 +1,24 @@
 <script setup>
 import { useProdctStore } from "../stores/Annonces";
+import { ref, computed } from 'vue'
 const products = useProdctStore();
+
+const filter = ref("")
+
+const filteredProducts = computed(() => {
+  return products.theStudents.filter(
+    stud => stud.Ville.toLowerCase().indexOf(filter.value.toLowerCase()) !== -1)
+})
+
 </script>
 
 <template>
     <h2>en dessous les annonces</h2>
-    <div class="Annonces" v-for="item in products.theStudents" :key="item.IdAnnonce">
-      {{ item.TitreAnnonce }}
-      {{item.DescriptionAnnonce}}
+    <input v-model="filter">
+    <div class="Annonces" v-for="item in filteredProducts" :key="item.IdAnnonce">
+      <span>{{ item.TitreAnnonce }}</span>
+      <span>{{item.DescriptionAnnonce}}</span>
+      <span>{{item.Ville}}</span>
     </div>
    
 </template>
@@ -18,5 +29,7 @@ const products = useProdctStore();
     padding: 20px;
     border: solid 2px black;
     margin-top: 12px;
+    display: flex;
+    flex-direction: column;
 }
 </style>
